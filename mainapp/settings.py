1a289da67398300b0ee38e8a6d927cf46bcab268
Manu S Rao <67941652+MANUSRAO@users.noreply.github.com>
2023-03-08 16:51:38 +0530

    pass

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,8 +33,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['172.17.10.180','localhost','172.17.8.228','https://newmain-production.up.railway.app','*']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'manusrao.is20@rvce.edu.in'
-EMAIL_HOST_PASSWORD = 'uzu1@buntu'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
