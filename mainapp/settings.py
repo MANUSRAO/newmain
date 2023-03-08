9e86c6d4b45f536f3556eb7a7cb88e2bbc486583
Manu S Rao <67941652+MANUSRAO@users.noreply.github.com>
2023-03-08 16:59:49 +0530

    Update settings.py

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -33,8 +33,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['172.17.10.180','localhost','172.17.8.228','https://newmain-production.up.railway.app','*']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'djangomailtemp@gmail.com'
+EMAIL_HOST_PASSWORD = 'drzffvderaltwqtp'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
